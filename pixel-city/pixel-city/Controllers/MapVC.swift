54df80b4ff7e7cbd71c647ec02440da60b048420
Octavian <octavian@192-168-0-101.rdsnet.ro>
2018-01-09 17:04:53 +0200

    first commit

---
+++ pixel-city/pixel-city/Controllers/MapVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  pixel-city
+//
+//  Created by Octavian on 09/01/2018.
+//  Copyright © 2018 Octavian. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
---
+++ pixel-city/pixel-city/Utilities/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  pixel-city
+//
+//  Created by Octavian on 09/01/2018.
+//  Copyright © 2018 Octavian. All rights reserved.
+//
+
+import Foundation
